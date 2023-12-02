953285a91a2859a0e99a82570145cf19bf68e453
Manon-Arc <112950709+Manon-Arc@users.noreply.github.com>
2023-12-02 13:43:17 +0100

    correction coutn protect mob

--- classes/mob.py
+++ classes/mob.py
@@ -4,13 +4,13 @@ from .dice import Dice
   
 class Goblini(Character):
         
-    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice) -> None:
-        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice)
+    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice) -> None:
+        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice)
         
     @staticmethod
     def create_default_character(name, template="default") -> Goblini | None:
         if (template=="default"):
-            return Goblini(name=name, classe="Goblini", max_health=10, attack_value=3, defense_value=0, attack_type_value=0, attack_special_value=0, touchable=0, dice=Dice(6))
+            return Goblini(name=name, classe="Goblini", max_health=10, attack_value=3, defense_value=0, attack_type_value=0, attack_special_value=0, touchable=0, count_protect=0, dice=Dice(6))
         return None
     def attack(self, target: Character):
         print(f"{self.get_name()} lance l'attaque Fureur du gobelin !\n")
@@ -32,13 +32,13 @@ class Goblini(Character):
 
 class Wolfor(Character):
   
-    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice) -> None:
-        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice)
+    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice) -> None:
+        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice)
         
     @staticmethod
     def create_default_character(name, template="default") -> Wolfor | None:
         if (template=="default"):
-            return Wolfor(name=name, classe="Wolfor", max_health=15, attack_value=4, defense_value=0, attack_type_value=0, attack_special_value=0, touchable=0, dice=Dice(6))
+            return Wolfor(name=name, classe="Wolfor", max_health=15, attack_value=4, defense_value=0, attack_type_value=0, attack_special_value=0, touchable=0, count_protect=0, dice=Dice(6))
         return None
     
     def attack(self, target: Character):
@@ -74,13 +74,13 @@ class Wolfor(Character):
     
 class Animal_trainer(Character):
 
-    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice) -> None:
-        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice)
+    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice) -> None:
+        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice)
         
     @staticmethod
     def create_default_character(name, template="default") -> Animal_trainer | None:
         if (template=="default"):
-            return Animal_trainer(name=name, classe="Animal_trainer", max_health=15, attack_value=5, defense_value=1, attack_type_value=0, attack_special_value=0, touchable=0, dice=Dice(6))
+            return Animal_trainer(name=name, classe="Animal_trainer", max_health=15, attack_value=5, defense_value=1, attack_type_value=0, attack_special_value=0, touchable=0, count_protect=0, dice=Dice(6))
         return None
     
     def attack(self, target: Character):
@@ -123,13 +123,13 @@ class Animal_trainer(Character):
 
 class Vampire(Character):
 
-    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice) -> None:
-        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice)
+    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice) -> None:
+        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice)
         
     @staticmethod
     def create_default_character(name, template="default") -> Vampire | None:
         if (template=="default"):
-            return Vampire(name=name, classe="Vampire", max_health=20, attack_value=5, defense_value=1, attack_type_value=0, attack_special_value=0, touchable=1, dice=Dice(6))
+            return Vampire(name=name, classe="Vampire", max_health=20, attack_value=5, defense_value=1, attack_type_value=0, attack_special_value=0, touchable=1, count_protect=0, dice=Dice(6))
         return None
     
     def attack(self, target: Character):
@@ -174,13 +174,13 @@ class Vampire(Character):
     
 class Hydre(Character):
 
-    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice) -> None:
-        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice)
+    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice) -> None:
+        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice)
         
     @staticmethod
     def create_default_character(name, template="default") -> Hydre | None:
         if (template=="default"):
-            return Hydre(name=name, classe="Hydre", max_health=30, attack_value=8, defense_value=3, attack_type_value=0, attack_special_value=0, touchable=0, dice=Dice(6))
+            return Hydre(name=name, classe="Hydre", max_health=30, attack_value=8, defense_value=3, attack_type_value=0, attack_special_value=0, touchable=0, count_protect=0, dice=Dice(6))
         return None
     
     def attack(self, target: Character):
@@ -214,13 +214,13 @@ class Hydre(Character):
 
 class Basilisc(Character):
 
-    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice) -> None:
-        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, dice)
+    def __init__(self, name: str, classe: str, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice) -> None:
+        super().__init__(name, classe, max_health, attack_value, defense_value, attack_type_value, attack_special_value, touchable, count_protect, dice)
         
     @staticmethod
     def create_default_character(name, template="default") -> Basilisc | None:
         if (template=="default"):
-            return Basilisc(name=name, classe="Basilisc", max_health=25, attack_value=6, defense_value=3, attack_type_value=0, attack_special_value=0, touchable=0, dice=Dice(6))
+            return Basilisc(name=name, classe="Basilisc", max_health=25, attack_value=6, defense_value=3, attack_type_value=0, attack_special_value=0, touchable=0, count_protect=0, dice=Dice(6))
         return None
     
     def attack(self, target: Character):
